be1320c7d51a9f60cd37774bc42405c713688d25
Jacek Kosinski <kashiash@gmail.com>
2024-01-11 00:45:44 +0100

    .

--- KursyWaluitNBP/DetailView.swift
+++ KursyWaluitNBP/DetailView.swift
@@ -12,24 +12,29 @@ struct DetailView: View {
     var rate: CurrencyARate
 
   @State  var rates: [DetailARate] = []
-    @State var sPadding: CGFloat = 4.2
-    @State var ePadding: CGFloat = 4.5
+
     var body: some View {
         Group{
             if rates.count > 0 {
-                Text(rate.id)
-                    .font(.title)
-                let costs = rates.map { $0.mid }
-                let min = costs.min()!
-                let max = costs.max()!
-                let strideBy = 5.0
 
+                let costs = rates.map { $0.mid }
+                let costMin:Double = costs.min()!
+                let costMax:Double = costs.max()!
+                let strideBy: Double = 6
+               //
                 VStack {
-                    if /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/ {
+                    HStack{
+                        Text(rate.id)
+                            .font(.title)
+                        Text(String(format:"%.4f",costMin))
+                        Text(String(format:"%.4f",costMax))
+                    }
+
+                    if  true {
                         Chart(rates) { item in
                         LineMark(
                             x: .value("Date", item.effectiveDate.toDate(.isoDate)!),
-                            y: .value("mid", item.mid/max)
+                            y: .value("mid", item.mid/costMax)
                         )
                         .interpolationMethod(.cardinal)
                         .foregroundStyle(.green)
@@ -38,22 +43,22 @@ struct DetailView: View {
                     }
                     .chartYAxis {
                         let defaultStride = Array(stride(from: 0, to: 1, by: 1.0/strideBy))
-                        let costsStride = Array(stride(from: min,
-                                                       through: max,
-                                                       by: (max - min)/strideBy))
+                        let costsStride = Array(stride(from: costMin,
+                                                       through: costMax,
+                                                       by: (costMax - costMin)/strideBy))
                         AxisMarks(position: .trailing, values: defaultStride) { axis in
                             AxisGridLine()
                             let value = costsStride[axis.index]
-                            AxisValueLabel("\(String(format: "%.2F", value)) PLN", centered: false)
+                            AxisValueLabel("\(String(format: "%.4F", value)) PLN", centered: false)
                         }
                     }
-//                    .chartForegroundStyleScale([
-//
-//                        "rate": .green,
-//                    ])
+                    .chartForegroundStyleScale([
+
+                        "rate": .green,
+                    ])
 
 
-                .frame(height: 100)
+                .frame(height:200)
                     } else {
                         /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
                     }
